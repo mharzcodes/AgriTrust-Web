780c5890a845e5cc3168727b7ab4f961faf3c398
Mharz <mharzshanelancajas@gmail.com>
2025-09-04 13:53:51 +0800

    for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/agritrust/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  base:'/AgriTrust-Web/'
 })
